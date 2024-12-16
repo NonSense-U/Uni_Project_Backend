d53e253ef4b780d55f69b35e12eb0e5713f590e0
NonSense <sy5964908@gmail.com>
2024-12-16 15:26:31 +0300

    batman

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+require __DIR__.'/auth.php';
+Route::post('/test',function()
+{
+dd('Cool!');
+});
