0829c2b77b78f322f9312a97e80a4d1a66e2300c
NonSense <sy5964908@gmail.com>
2024-12-24 16:53:24 +0300

    GoogleDrive Storage Access

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Middleware\RBAC;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 foreach(glob(dirname(__FILE__).'/*/*', GLOB_NOSORT) as $route_file){
     include $route_file;
@@ -15,5 +17,30 @@ require __DIR__.'/auth.php';
 
 Route::get('/test',function()
 {
-dd('2!');
+    try {
+        $files = Storage::disk('google')->allFiles();
+        return response()->json($files);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+})->middleware('auth');
+
+Route::post('/upload',function(Request $request)
+{
+    $request->validate([
+        'file' => 'required|file|mimes:jpg,png,pdf,docx|max:2048',
+    ]);
+
+    // Get the uploaded file
+    $file = $request->file('file');
+
+    // Set the Google Drive path
+    // $googleDrivePath = 'uploads/' . $file->getClientOriginalName();
+
+    // Upload the file
+    if (Storage::disk('google')->put('pic.jpg', file_get_contents($file))) {
+         dd("File uploaded successfully!");
+    } else {
+        dd ("Failed to upload file.");
+    }
 });
